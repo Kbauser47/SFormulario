6ecfafb500ac7c7b0c671e4b2c379597a5010a72
cabarc750 <cabarc750@gmail.com>
2022-10-05 02:25:35 -0400

    Agregar archivos de proyecto.

---
+++ PFormulario/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace PFormulario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnom = new System.Windows.Forms.TextBox();
+            this.txtapel = new System.Windows.Forms.TextBox();
+            this.txtcar = new System.Windows.Forms.TextBox();
+            this.btnnuevo = new System.Windows.Forms.Button();
+            this.btnexistente = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtcar);
+            this.groupBox1.Controls.Add(this.txtapel);
+            this.groupBox1.Controls.Add(this.txtnom);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(407, 174);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SumaDeNumeros";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese Nombre :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese Apellido :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingrese Carrera :";
+            // 
+            // txtnom
+            // 
+            this.txtnom.Location = new System.Drawing.Point(175, 29);
+            this.txtnom.Name = "txtnom";
+            this.txtnom.Size = new System.Drawing.Size(203, 20);
+            this.txtnom.TabIndex = 3;
+            // 
+            // txtapel
+            // 
+            this.txtapel.Location = new System.Drawing.Point(175, 79);
+            this.txtapel.Name = "txtapel";
+            this.txtapel.Size = new System.Drawing.Size(203, 20);
+            this.txtapel.TabIndex = 4;
+            // 
+            // txtcar
+            // 
+            this.txtcar.Location = new System.Drawing.Point(175, 126);
+            this.txtcar.Name = "txtcar";
+            this.txtcar.Size = new System.Drawing.Size(151, 20);
+            this.txtcar.TabIndex = 5;
+            // 
+            // btnnuevo
+            // 
+            this.btnnuevo.Location = new System.Drawing.Point(182, 192);
+            this.btnnuevo.Name = "btnnuevo";
+            this.btnnuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnnuevo.TabIndex = 1;
+            this.btnnuevo.Text = "Nuevo";
+            this.btnnuevo.UseVisualStyleBackColor = true;
+            // 
+            // btnexistente
+            // 
+            this.btnexistente.Location = new System.Drawing.Point(263, 192);
+            this.btnexistente.Name = "btnexistente";
+            this.btnexistente.Size = new System.Drawing.Size(75, 23);
+            this.btnexistente.TabIndex = 2;
+            this.btnexistente.Text = "Existente";
+            this.btnexistente.UseVisualStyleBackColor = true;
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Location = new System.Drawing.Point(344, 192);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(75, 23);
+            this.btnsalir.TabIndex = 3;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 235);
+            this.Controls.Add(this.btnnuevo);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnexistente);
+            this.Controls.Add(this.btnsalir);
+            this.Name = "Form1";
+            this.Text = "Registro";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtcar;
+        private System.Windows.Forms.TextBox txtapel;
+        private System.Windows.Forms.TextBox txtnom;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnnuevo;
+        private System.Windows.Forms.Button btnexistente;
+        private System.Windows.Forms.Button btnsalir;
+    }
+}
+
